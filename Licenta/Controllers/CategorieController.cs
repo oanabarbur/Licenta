56909324529cbf2764c211c25d78585c364d94f5
Oana <oana_barbur@yahoo.com>
2020-04-12 19:46:27 +0300

    Initial Commit

---
+++ Licenta/Controllers/CategorieController.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Licenta.Models;
+
+namespace Licenta.Controllers
+{
+    public class CategorieController : Controller
+    {
+        private ApplicationDbContext _context;
+
+        public CategorieController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            _context.Dispose();
+        }
+        // GET: Categorie
+        public ViewResult Index()
+        {
+            var categorie = _context.Categorie.ToList();
+            return View(categorie);
+        }
+
+        public ActionResult Detalii(int id)
+        {
+            var categorie = _context.Customers.SingleOrDefault(c => c.Id == id);
+
+            if (categorie == null)
+                return HttpNotFound();
+
+            return View(categorie);
+        }
+    
+        
+    }
+
+    internal class ApplicationDbContext
+    {
+        public ApplicationDbContext()
+        {
+        }
+
+        public object Categorie { get; internal set; }
+        public object Customers { get; internal set; }
+
+        internal void Dispose()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Licenta/ViewModels/RandomEchipamenteViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Licenta.ViewModels
+{
+    public class RandomEchipamenteViewModel
+    {
+   //     public Echipamente Echipamente { get; set; }
+ //       public List<Categorie> Categorie { get; set; }
+    }
+}
